c3eda58b6eb1967262c4944167906ec188ee6318
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-06-30 10:18:14 +0700

    [REMOVE] SCB_VTOR in led's header

--- examples/00-minimal/bm_led_blink.h
+++ examples/00-minimal/bm_led_blink.h
@@ -6,7 +6,6 @@
 #define RCC_AHBENR (*(volatile uint32_t*)0x4002381C)
 #define GPIOB_MODER (*(volatile uint32_t*)0x40020400)
 #define GPIOB_BSRR (*(volatile uint32_t*)0x40020418)
-#define SCB_VTOR (*(volatile uint32_t*)0xE000ED08)
 
 #define LED_PIN 8
 
