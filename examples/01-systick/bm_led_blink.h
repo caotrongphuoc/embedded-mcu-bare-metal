5a356a9406b9bd4a85b5c72d84ca273ab8f15a52
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-06-30 11:28:59 +0700

    [ADD] system registers and system clock into header

--- examples/01-systick/bm_led_blink.h
+++ examples/01-systick/bm_led_blink.h
@@ -3,10 +3,21 @@
 
 #include <stdint.h>
 
+// STM32 peripherals
 #define RCC_AHBENR (*(volatile uint32_t*)0x4002381C)
 #define GPIOB_MODER (*(volatile uint32_t*)0x40020400)
 #define GPIOB_BSRR (*(volatile uint32_t*)0x40020418)
 
+// Cortex-M3 system registers
+#define SYSTICK_CTRL (*(volatile uint32_t*)0xE000E010)
+#define SYSTICK_LOAD (*(volatile uint32_t*)0xE000E014)
+#define SYSTICK_VAL (*(volatile uint32_t*)0xE000E018)
+
+// LED pin configuration
 #define LED_PIN 8
+
+// System clock and tick configuration
+#define SYSCLK_HZ 2097000U
+#define TICK_HZ 1000U
 
 #endif // __BM_LED_BLINK_H__
