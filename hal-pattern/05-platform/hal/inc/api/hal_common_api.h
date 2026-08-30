5680a42fa11ecace214991c944703da1f2a81c31
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-08-30 23:27:49 +0700

    [HAL] add common API definitions

---
+++ hal-pattern/05-platform/hal/inc/api/hal_common_api.h
@@ -0,0 +1,38 @@
+#ifndef HAL_COMMON_API_H
+#define HAL_COMMON_API_H
+
+#ifdef __cplusplus
+#define HAL_HEADER    extern "C" {
+#define HAL_FOOTER    }
+#else
+#define HAL_HEADER
+#define HAL_FOOTER
+#endif
+
+#define HAL_PARAMETER_NOT_USED(p)    ((void) (p))
+
+HAL_HEADER
+
+/** Return values shared by HAL modules. */
+typedef enum e_hal_err
+{
+    HAL_SUCCESS = 0,
+    HAL_ERR_ASSERTION,
+    HAL_ERR_INVALID_POINTER,
+    HAL_ERR_INVALID_ARGUMENT,
+    HAL_ERR_INVALID_SIZE,
+    HAL_ERR_INVALID_CHANNEL,
+    HAL_ERR_INVALID_MODE,
+    HAL_ERR_UNSUPPORTED,
+    HAL_ERR_NOT_OPEN,
+    HAL_ERR_NOT_ENABLED,
+    HAL_ERR_IN_USE,
+    HAL_ERR_HW_LOCKED,
+    HAL_ERR_ALREADY_OPEN,
+    HAL_ERR_TIMEOUT,
+    HAL_ERR_INVALID_STATE
+} hal_err_t;
+
+HAL_FOOTER
+
+#endif /* HAL_COMMON_API_H */
