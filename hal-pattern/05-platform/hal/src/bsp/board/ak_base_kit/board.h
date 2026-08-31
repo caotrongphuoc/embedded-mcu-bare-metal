3c7e4d426d289c714bbf2f4a232c33b63bc09de6
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-08-31 20:46:00 +0700

    [BSP] add AK Base Kit LED information

---
+++ hal-pattern/05-platform/hal/src/bsp/board/ak_base_kit/board.h
@@ -0,0 +1,8 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#include "board_leds.h"
+
+#define BOARD_AK_BASE_KIT
+
+#endif // __BOARD_H__
---
+++ hal-pattern/05-platform/hal/src/bsp/board/ak_base_kit/board_leds.c
@@ -0,0 +1,17 @@
+#include "board.h"
+#include "pin_data.h"
+
+#if defined(BOARD_AK_BASE_KIT)
+
+static const hal_gpio_pin_t g_bsp_prv_leds[] =
+{
+    AK_BASE_KIT_LED_PIN
+};
+
+const bsp_leds_t g_bsp_leds =
+{
+    .led_count = (uint16_t) (sizeof(g_bsp_prv_leds) / sizeof(g_bsp_prv_leds[0])),
+    .p_leds    = &g_bsp_prv_leds[0]
+};
+
+#endif
