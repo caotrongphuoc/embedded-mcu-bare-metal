099785d955d2d3b6de84b94cb5021100ddc8e94e
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-08-31 18:44:38 +0700

    [HAL] add STM32L1 GPIO pin decoding

--- hal-pattern/05-platform/hal/src/mcu/stm32l1/gpio/stm32l1_gpio.c
+++ hal-pattern/05-platform/hal/src/mcu/stm32l1/gpio/stm32l1_gpio.c
@@ -1,5 +1,9 @@
 #include "stm32l1_gpio.h"
 
+#define STM32L1_GPIO_PRV_PORT_OFFSET    (8U)
+#define STM32L1_GPIO_PRV_PORT_BITS      (0xFF00U)
+#define STM32L1_GPIO_PRV_PIN_BITS       (0x00FFU)
+
 const hal_gpio_api_t g_gpio_on_stm32l1_gpio =
 {
     .open             = STM32L1_GPIO_Open,
