55838e2dd7f4e1ff4ea433898537e54618ee8174
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-09-01 01:24:29 +0700

    [BSP] validate STM32L1 clock configuration

--- hal-pattern/05-platform/hal/src/bsp/mcu/stm32l1/bsp_clocks.c
+++ hal-pattern/05-platform/hal/src/bsp/mcu/stm32l1/bsp_clocks.c
@@ -1,6 +1,26 @@
 #include "bsp_clocks.h"
 #include "stm32l1xx.h"
 
+#if BSP_CFG_XTAL_HZ == 0
+ #error "BSP_CFG_XTAL_HZ must be greater than zero"
+#endif
+
+#if BSP_CFG_PLL_MUL == 0
+ #error "BSP_CFG_PLL_MUL must be greater than zero"
+#endif
+
+#if BSP_CFG_PLL_DIV == 0
+ #error "BSP_CFG_PLL_DIV must be greater than zero"
+#endif
+
+#if BSP_CFG_SYSCLK_HZ != ((BSP_CFG_XTAL_HZ * BSP_CFG_PLL_MUL) / BSP_CFG_PLL_DIV)
+ #error "BSP_CFG_SYSCLK_HZ does not match PLL configuration"
+#endif
+
+#if BSP_CFG_SYSCLK_HZ > 32000000
+ #error "STM32L151 system clock exceeds 32 MHz"
+#endif
+
 /** Initializes the system clocks. */
 void bsp_clock_init(void)
 {
