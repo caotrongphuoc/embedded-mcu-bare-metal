d41e3f48433a5bb68024c0072c6168a689cd4e7b
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-09-04 22:52:51 +0700

    [CFG] add STM32L1 UART driver config template

---
+++ hal-pattern/05-platform/examples/ak_base_kit/uart/hello/hal_cfg/driver/stm32l1_uart_cfg.h
@@ -0,0 +1,12 @@
+#ifndef __STM32L1_UART_CFG_H__
+#define __STM32L1_UART_CFG_H__
+
+#if __has_include("bsp_cfg.h")
+#include "bsp_cfg.h"
+#endif
+
+#ifndef STM32L1_UART_CFG_PARAM_CHECKING_ENABLE
+#define STM32L1_UART_CFG_PARAM_CHECKING_ENABLE    (BSP_CFG_PARAM_CHECKING_ENABLE)
+#endif
+
+#endif // __STM32L1_UART_CFG_H__
